3452c0f6a2fe49cb5c54da1f88e2a06b745f7291
landonbu <brandonbl2021@gmail.com>
2022-03-29 14:04:08 -0400

    meta title

--- src/Pages/Tii.js
+++ src/Pages/Tii.js
@@ -13,7 +13,7 @@ export default function BlackJack(props){
         <div className="home--projects">
             <p className="title2">THE INTERNSHIP INITIATIVE</p>
             <div className="page--text--body">
-                &emsp;&emsp;The Internship Initiative was a web application me and my friends made during the COVID-19 pandemic. The 
+                &emsp;&emsp;<av href="https://interninit.com">The Internship Initiative</av> was a web application me and my friends made during the COVID-19 pandemic. The 
                 purpose of the application was to present high school students with work experience opportunities. The application is split up
                 into two sections: the student portal and the business portal. Students send their applications to companies using the student portal, which are then
                 showcased on a company's business portal. From there, the company can start the hiring process. 
@@ -23,8 +23,7 @@ export default function BlackJack(props){
                 no more than simple console programs. Before this project, I never touched JavaScript and I had no experience in web development, so it was an interesting experience for me to
                 learn as I built the platform. For the most part, I handled the frontend development for the application. I created numerous Figma designs before choosing one to implement. In total I spent 
                 four months developing the two portals, splitting the work with my friend.
-                By the end, we created and deployed the web application and marketed it towards high schools across New England. While over 50 schools expressed interest in the platform, it currently
-                remains unused.
+                By the end, we created and deployed the web application and marketed it towards high schools across New England. While over 50 schools expressed interest in the platform, it remains unpopulated.
                 <br/>
                 <br/>
                 <strong>Technologies Used: </strong> ReactJS, HTML, CSS, JavaScript, Node.js, Redux, Figma, DynamoDB, AWS
